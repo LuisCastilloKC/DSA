18f18c15a32beca48c0a7834f2a0b9881117a1e0
Luis Castillo <mr.luiscastillo@gmail.com>
2021-12-22 23:27:10 -0500

    breaking problem solving Algo

---
+++ Algorithms_and_Data_Structures_Masterclass/breakItDowm.js
@@ -0,0 +1,38 @@
+//Write a function which takes in a string and returns counts of each character in the string.
+
+charCount("aaaa");
+/*{
+    1: 1,
+    2: 1,
+    3: 1,
+    4: 1,
+    b: 1,
+    e: 1,
+    i: 2,
+    m: 1,
+    n: 2,
+    o: 1,
+    p: 1,
+    r: 2,
+    s: 1,
+    u: 2,
+    y: 1
+} */
+charCount("hello");
+charCount("Your PIN number is 1234!");
+
+function charCount(str){
+    //do something
+    // return an object with keys that are lowercase alphanumeric characters in the strin; value
+}
+
+function charCount(str){
+    // make object to return at end
+    // loop over string, for each character...
+        // if the car is a  number/letter AND is a *key in object, add one to count
+        // if the char is number/letter AND-* not in object, add it to object and set value to 1
+        // if character is something else (space, period, etc.) don't do anythiung
+    
+
+    // return object at end
+}
