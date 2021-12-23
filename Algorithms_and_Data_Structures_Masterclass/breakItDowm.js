10775cffa38c63942375ea08785d741f45e35661
Luis Castillo <mr.luiscastillo@gmail.com>
2021-12-22 23:47:40 -0500

    add some more code to algo

--- Algorithms_and_Data_Structures_Masterclass/breakItDowm.js
+++ Algorithms_and_Data_Structures_Masterclass/breakItDowm.js
@@ -1,6 +1,17 @@
 //Write a function which takes in a string and returns counts of each character in the string.
 
 charCount("aaaa");
+/* {
+    a: 4
+} */
+charCount("hello");
+/* {
+    h: 1,
+    e: 1,
+    l: 2,
+    o: 1
+}*/
+charCount("Your PIN number is 1234!");
 /*{
     1: 1,
     2: 1,
@@ -18,8 +29,6 @@ charCount("aaaa");
     u: 2,
     y: 1
 } */
-charCount("hello");
-charCount("Your PIN number is 1234!");
 
 function charCount(str){
     //do something
@@ -28,11 +37,20 @@ function charCount(str){
 
 function charCount(str){
     // make object to return at end
+    let result = {};
     // loop over string, for each character...
+    for(let i = 0; i < str.length; i++){
+        let char = str[i]
         // if the car is a  number/letter AND is a *key in object, add one to count
-        // if the char is number/letter AND-* not in object, add it to object and set value to 1
+        if(result[char] > 0){
+            result[char]++;
+        }
+            // if the char is number/letter AND-* not in object, add it to object and set value to 1
+            else { 
+                result[char] = 1;
+        }
+    }
         // if character is something else (space, period, etc.) don't do anythiung
-    
-
     // return object at end
+    return result;
 }
