f605ea817d0dd6ef5520208b08c1c44f393c2826
Luis Castillo <mr.luiscastillo@gmail.com>
2022-01-24 23:29:44 -0500

    isPrime(refactor)

--- Algorithms_and_Data_Structures_Masterclass/structy/isPrime.js
+++ Algorithms_and_Data_Structures_Masterclass/structy/isPrime.js
@@ -49,3 +49,22 @@ const isPrime = (n) => {
     }
     return true
   };
+
+
+
+
+  //--------------------------
+
+  const isPrime = (n) => {
+    if (n < 2) return false;
+    
+    for (let i = 2; i <= Math.sqrt(n); i += 1) {
+      if (n % i === 0) return false;
+    }
+    
+    return true;
+  };
+
+// n = input number
+// Time: O(square_root(n))
+// Space: O(1)
