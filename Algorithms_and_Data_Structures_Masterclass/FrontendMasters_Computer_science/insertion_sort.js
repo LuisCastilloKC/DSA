f1894210cb559ebbd94da254c2702acc95a225fa
Luis Castillo <mr.luiscastillo@gmail.com>
2022-03-04 23:37:38 -0500

    insertion with while loop

--- Algorithms_and_Data_Structures_Masterclass/FrontendMasters_Computer_science/insertion_sort.js
+++ Algorithms_and_Data_Structures_Masterclass/FrontendMasters_Computer_science/insertion_sort.js
@@ -31,3 +31,22 @@ function insertionSort(nums) {
 
 //   The Big(O) is O(n^2)
 //   Space Complexity O(1)
+
+//--------------------------------------------------------------------------
+
+
+
+let insertionSort2 = (arr) => {
+    let length = arr.length;
+    for (let i = 1; i < length; i++) {
+        let key = arr[i];
+        let j = i - 1;
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+    return arr;
+};
+console.log(insertionSort2([21,24,25,29,27,22,23,20]))
