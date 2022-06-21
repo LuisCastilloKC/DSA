12f57420470b36698f4b251a39075844650c7ce4
Luis Castillo <mr.luiscastillo@gmail.com>
2022-06-20 23:36:19 -0400

    the nai solution

--- Algorithms_and_Data_Structures_Masterclass/colt-steel-Algo/two_pointers.js
+++ Algorithms_and_Data_Structures_Masterclass/colt-steel-Algo/two_pointers.js
@@ -1,4 +1,24 @@
 
+// The Nai solution
+function sumZero(arr){
+    for(let i = 0; i <arr.length; i++){
+        for(let j = i+1; j< arr.length; j++){
+            if(arr[i] + arr[j] === 0){
+                return [arr[i], arr[j]]
+            }
+        }
+    }
+}
+
+//Time Complexity - O(N*2)
+//Space Complexity - O(1)
+
+sumZero([-4,-3,-2,-1,0,1,2,5])
+
+// -------------------------------------------------------------------
+
+// The Best solution
+
 console.log(sumZero([-4,-3,-2,-1,0,1,2,3,10]))
 
 function sumZero(arr){
@@ -17,3 +37,4 @@ function sumZero(arr){
         console.log(sum)
     }
 }
+
