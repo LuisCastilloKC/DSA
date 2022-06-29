c5a19b016a00f4bc67d6badf663621e8bfbb132a
Luis Castillo <mr.luiscastillo@gmail.com>
2022-06-28 23:59:26 -0400

    DSA

---
+++ leetcode/n.js
@@ -0,0 +1,35 @@
+// Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+
+// An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+ 
+
+// Example 1:
+
+// Input: strs = ["eat","tea","tan","ate","nat","bat"]
+// Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+// Example 2:
+
+// Input: strs = [""]
+// Output: [[""]]
+// Example 3:
+
+// Input: strs = ["a"]
+// Output: [["a"]]
+
+const groupAnagrams = strs => {
+    const map = {};
+    
+    for (let str of strs) {
+        const key = [...str].sort().join('');
+
+        if (!map[key]) {
+            map[key] = [];
+        }
+
+        map[key].push(str);
+    }
+    
+    return Object.values(map);
+};
+
